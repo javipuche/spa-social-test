edd0494ebb2354218f622ac5c55f9b55e6375f0c
Javier Puche <javierpuchevictoria@gmail.com>
2024-02-28 16:40:28 +0100

    fist commit

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import HelloWorld from "./components/HelloWorld.vue";
 
 useSeoMeta({
   title: "About",
-  description: "My about page",
+  description: "My about pageeeee",
   ogDescription: "Still about my about page",
   ogTitle: "About",
   ogImage: "/images/vite.svg",
